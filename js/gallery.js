06130fcfbb096b4174f44d07870a7726a62229d6
Anna <anna.foerster1@gmx.net>
2019-06-20 15:14:40 +0200

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,28 @@
 function activateGallery(){
-	alert("Hello from the gallery file!");
+	//let thumbnails = document.querySelector("#gallery-thumbs").querySelectorAll("img");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage = document.querySelector("#gallery-photo img");
+
+	thumbnails.forEach(function(thumbnail) {
+		thumbnail.addEventListener("click", function(){
+			//Set clicked image as main image.
+			let newImageSrc = thumbnail.dataset.largeVersion;
+			let newImageAlt = thumbnail.alt;
+			mainImage.setAttribute("src", newImageSrc);
+			mainImage.setAttribute("alt", newImageAlt);
+
+			//Change which image is current.
+			let currentClass = "current";
+			document.querySelector(".current").classList.remove(currentClass);
+			thumbnail.parentNode.classList.add(currentClass);
+
+			//Update image infos
+			let galleryInfo = document.querySelector("#gallery-info");
+			let title = galleryInfo.querySelector(".title");
+			let description = galleryInfo.querySelector(".description");
+
+			title.innerHTML = thumbnail.dataset.title;
+			description.innerHTML = thumbnail.dataset.description;
+		});
+	});
 }
